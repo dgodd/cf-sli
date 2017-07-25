804c73534cd3ccca7470474367665988fdc9c02f
Dave Goddard <dave@goddard.id.au>
2017-07-25 16:05:33 -0400

    Sample app to not have dependencies

--- assets/ruby_simple/main.rb
+++ assets/ruby_simple/main.rb
@@ -1,25 +1,6 @@
-require "roda"
+require "webrick"
 STDOUT.sync = true
 STDERR.sync = true
-
-class App < Roda
-  route do |r|
-    r.root do
-<<-RESPONSE
-Healthy
-It just needed to be restarted!
-My application metadata: #{ENV['VCAP_APPLICATION']}
-My port: #{ENV['PORT']}
-My custom env variable: #{ENV['CUSTOM_VAR']}
-RESPONSE
-    end
-
-    r.get 'log', String do |message|
-      STDOUT.puts(message)
-      "logged #{message} to STDOUT"
-    end
-  end
-end
 
 Thread.new do
   while true do
@@ -27,3 +8,45 @@ Thread.new do
     sleep 1
   end
 end
+
+class EnvServlet < WEBrick::HTTPServlet::AbstractServlet
+    def do_GET (request, response)
+      if request.unparsed_uri == '/'
+        response.status = 200
+        response.body = <<-RESPONSE
+Healthy
+It just needed to be restarted!
+My application metadata: #{ENV['VCAP_APPLICATION']}
+My port: #{ENV['PORT']}
+My custom env variable: #{ENV['CUSTOM_VAR']}
+RESPONSE
+      else
+        response.status = 404
+      end
+    end
+end
+
+class LogServlet < WEBrick::HTTPServlet::AbstractServlet
+    def do_GET (request, response)
+      m = request.unparsed_uri.match(/^\/log\/(.*)/)
+      if m && m[1]
+        message = m[1]
+        response.status = 200
+        STDOUT.puts(message)
+        response.body = "logged #{message} to STDOUT"
+      else
+        response.status = 404
+      end
+    end
+end
+
+server = WEBrick::HTTPServer.new(:Port => 1234)
+
+server.mount "/log", LogServlet
+server.mount "/", EnvServlet
+
+trap("INT") {
+    server.shutdown
+}
+
+server.start
